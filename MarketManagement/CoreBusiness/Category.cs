9e5d5968b165a60478ea237707a5ad60a9f993e9
NevenaDarijevic <darijevicnevena00@gmail.com>
2022-01-05 11:39:23 +0100

    Added everything for adding new category

--- MarketManagement/CoreBusiness/Category.cs
+++ MarketManagement/CoreBusiness/Category.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoreBusiness
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
         public String Name { get; set; }
         public String Description { get; set; }
     }
--- MarketManagement/WebApplication/Startup.cs
+++ MarketManagement/WebApplication/Startup.cs
@@ -38,6 +38,8 @@ namespace WebApplication
 
             //Dependency injection for Use Cases and Repositories
             services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
+
+            services.AddTransient<IAddCategoryUseCase, AddCategoryUseCase>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
